d522cfd4e7416a83346475c1765b170541ebf54f
aangelos28 <aangelos28@gmail.com>
2021-07-08 12:51:40 -0400

    Add documentation to mandelbrot.py

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,3 +1,7 @@
+"""
+Methods for generating the mandelbrot fractal.
+"""
+
 import math
 
 import numba
@@ -8,6 +12,26 @@ from PIL import Image as im
 
 def generate_mandelbrot(width=1200, height=800, max_iterations=50, re_start=-2.0, re_end=1.0, im_start=-1.0, im_end=1.0,
                         color_hue=204, color_saturation=0.64, color_intensity=1.0, use_gpu=True):
+    """
+    Generate a mandelbrot visualization.
+
+    Args:
+        width: Width of the image in pixels
+        height: Height of the image in pixels
+        max_iterations: Max iterations for orbital escape
+        re_start: Minimum value of the real complex plane
+        re_end: Maximum value of the real complex plane
+        im_start: Minimum value of the imaginary complex plane
+        im_end: Maximum value of the imaginary complex plane
+        color_hue: Hue of the color used for the visualization
+        color_saturation: Saturation of the color used for the visualization
+        color_intensity: Intensity of the color used for the visualization
+        use_gpu: Whether to use CUDA to compute the mandelbrot
+
+    Returns:
+        Pixels of the visualization and a Pillow image
+    """
+
     pixels = np.zeros([width, height, 3], dtype=np.uint8)
 
     if use_gpu:
@@ -29,6 +53,24 @@ def generate_mandelbrot(width=1200, height=800, max_iterations=50, re_start=-2.0
 @numba.jit(nopython=True, parallel=True)
 def __mandelbrot_cpu(pixels, width, height, max_iterations, re_start, re_end, im_start, im_end, color_hue,
                      color_saturation, color_intensity):
+    """
+    Generate a mandelbrot visualization using multi-threading.
+
+    Args:
+        pixels: Reference to the RGB pixel array
+        width: Width of the image in pixels
+        height: Height of the image in pixels
+        max_iterations: Max iterations for orbital escape
+        re_start: Minimum value of the real complex plane
+        re_end: Maximum value of the real complex plane
+        im_start: Minimum value of the imaginary complex plane
+        im_end: Maximum value of the imaginary complex plane
+        color_hue: Hue of the color used for the visualization
+        color_saturation: Saturation of the color used for the visualization
+        color_intensity: Intensity of the color used for the visualization
+        use_gpu: Whether to use CUDA to compute the mandelbrot
+    """
+
     for x in prange(0, width):
         for y in prange(0, height):
             c = complex((re_start + (x / width) * (re_end - re_start)), (im_start + (y / height) * (im_end - im_start)))
@@ -55,6 +97,24 @@ def __mandelbrot_cpu(pixels, width, height, max_iterations, re_start, re_end, im
 @cuda.jit
 def __mandelbrot_cuda(pixels, width, height, max_iterations, re_start, re_end, im_start, im_end, color_hue,
                       color_saturation, color_intensity):
+    """
+    Generate a mandelbrot visualization using CUDA.
+
+    Args:
+        pixels: Reference to the RGB pixel array
+        width: Width of the image in pixels
+        height: Height of the image in pixels
+        max_iterations: Max iterations for orbital escape
+        re_start: Minimum value of the real complex plane
+        re_end: Maximum value of the real complex plane
+        im_start: Minimum value of the imaginary complex plane
+        im_end: Maximum value of the imaginary complex plane
+        color_hue: Hue of the color used for the visualization
+        color_saturation: Saturation of the color used for the visualization
+        color_intensity: Intensity of the color used for the visualization
+        use_gpu: Whether to use CUDA to compute the mandelbrot
+    """
+
     x, y = cuda.grid(2)
 
     if x < pixels.shape[0] and y < pixels.shape[1]:
